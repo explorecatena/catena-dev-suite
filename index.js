fa356db1ed10dbbe6de331266a31e480edbfdbeb
Dylan Seago <dylan.seago@gmail.com>
2018-03-13 14:57:08 -0400

    Add pages for home, editor and ganache

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require('http')
 const path = require('path')
 const proxy = require('http-proxy-middleware')
 const { fork } = require('child_process')
+const nunjucks = require('nunjucks')
 
+const isDev = process.env.NODE_ENV === 'development'
 const PORT = process.env.PORT || 8080
 const GANACHE_PORT = PORT + 1
 
@@ -22,17 +24,39 @@ ganacheProcess.on('error', (e) => {
 const ganacheUrl = `http://localhost:${GANACHE_PORT}`
 
 const app = express()
-app.use(['/explorer*', '/assets*', '/wss'], proxy({
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: isDev,
+  noCache: isDev
+})
+
+app.use((req, res, next) => {
+  res.locals.env = req.app.get('env')
+  res.locals.req = req
+  const lng = req.query.lng || 'en'
+  res.locals.lng = lng
+  res.locals.nextLng = lng === 'en' ? 'fr' : 'en'
+  next()
+})
+
+app.use('/static', express.static(path.resolve(__dirname, 'static')))
+app.use('/img', express.static(path.resolve(__dirname, 'static/img')))
+
+app.use('/remix', express.static(path.join(libDir, 'remix-ide')))
+app.use(['/ganache', '/ganache.*.js', '/assets*', '/wss'], proxy({
   target: ganacheUrl,
   ws: true,
   pathRewrite: {
-    '^/explorer$': '/'
+    '^/ganache$': '/'
   }
 }))
-app.use('/editor', express.static(path.join(libDir, 'remix-ide')))
-app.use('/static', express.static(path.resolve(__dirname, 'static')))
-app.use('/img', express.static(path.resolve(__dirname, 'static/img')))
-app.use(express.static(path.resolve(__dirname, 'views')))
+
+app.get('/explorer', (req, res) => res.render('iframed.njk', { iframeSrc: '/ganache' }))
+app.get('/editor', (req, res) => res.render('iframed.njk', { iframeSrc: '/remix' }))
+app.get('/docs', (req, res) => res.render('docs.njk'))
+app.get('/', (req, res) => res.render('index.njk'))
 
 const server = http.createServer(app)
 
